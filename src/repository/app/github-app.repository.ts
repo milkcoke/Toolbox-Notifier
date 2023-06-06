0777a1aa0377b3aefafc2e817cf3a7c9ec63aad0
milkcoke <mbh023@gmail.com>
2023-06-07 06:53:53 +0900

    feat: Add GitHubAppRepository class

---
+++ src/repository/app/github-app.repository.ts
@@ -0,0 +1,66 @@
+import {injectable, singleton} from 'tsyringe'
+import {IAppRepository} from './app.repository.interface'
+import {App} from '../../domain/app/app'
+import {Octokit} from '@octokit/rest'
+import Config from '../../../config/config'
+import {TAppReleaseInfo} from '../../types/app/appRelease'
+
+@singleton()
+@injectable()
+export class GithubAppRepository implements IAppRepository {
+  private readonly _octokit = new Octokit()
+  private readonly _owner: string
+  private readonly _repo: string
+  constructor(private readonly _config: Config) {
+    this._owner = this._config.owner
+    this._repo = this._config.repo
+  }
+  async findLatestApp(): Promise<App> {
+    const releaseId = await this._getLatestReleaseID()
+    const version = await this._getLatestVersion()
+    const appReleaseInfos = await this._getNumOfDownload(releaseId)
+    // TODO: LatestApp 이 무엇인지 정의하기
+    return new App('name', version, 'Windows-x64', 5000)
+  }
+
+  async findSecondLatestApp(): Promise<App> {
+    return Promise.resolve(undefined)
+  }
+
+  private async _getLatestReleaseID(): Promise<number> {
+
+    const latestRelease = await this._octokit.rest.repos.getLatestRelease({
+      owner: this._owner,
+      repo: this._repo
+    })
+
+    console.dir(latestRelease)
+
+    return latestRelease.data.id
+  }
+
+  private async _getLatestVersion(): Promise<string> {
+    const latestRelease = await this._octokit.rest.repos.getLatestRelease({
+      owner: this._owner,
+      repo: this._repo
+    })
+
+    return latestRelease.data.tag_name
+  }
+  private async _getNumOfDownload(releaseId: number): Promise<TAppReleaseInfo[]> {
+
+    const response = await this._octokit.rest.repos.getRelease({
+      owner: this._owner,
+      repo: this._repo,
+      release_id: releaseId
+    })
+
+    return response.data.assets.map(asset=>{
+      return {
+        fullAppName: asset.name,
+        downloadCount: asset.download_count
+      }
+    })
+  }
+
+}
