0a2cd4c3718041a0b18f50055c8b00a1f314ff78
milkcoke <mbh023@gmail.com>
2023-06-06 16:21:40 +0900

    feat: Add App domain
    Define app class.

---
+++ src/domain/app/app.ts
@@ -0,0 +1,15 @@
+import {TOperatingSystem} from '../../constant/operatingSystem'
+
+export class App {
+  private readonly _name: string
+  private readonly _version: string
+  private readonly _operatingSystem: TOperatingSystem
+  private readonly _downloadCount: number
+
+  constructor(name: string, version: string, operatingSystem: TOperatingSystem, downloadCount) {
+    this._name = name
+    this._version = version
+    this._operatingSystem = operatingSystem
+    this._downloadCount = downloadCount
+  }
+}
