6154ee5439619e9cb6bb54854cfcd45ad27366ff
milkcoke <mbh023@gmail.com>
2023-07-15 04:19:18 +0900

    feat: Redefine IAppService interface

--- src/repository/app/app.repository.interface.ts
+++
@@ -1,8 +0,0 @@
-import {App} from '../../domain/app/app'
-
-export interface IAppRepository {
-  // FIXME: Is it appropriate approach return type domain?
-  // If domain is complex then define domain DTO class
-  findLatestApp(): Promise<App>
-  findSecondLatestApp(): Promise<App>
-}
--- src/service/app/app.service.interface.ts
+++ src/service/app/app.service.interface.ts
@@ -2,6 +2,7 @@ import {App} from '../../domain/app/app'
 import {TAppDiff} from '../../types/app/appDiff'
 
 export interface IAppService {
-  getAppInfo(): Promise<App>
-  getDiffDownloadCount(prevApp: App, curApp: App): TAppDiff
+  readCurrentAppsInfo(): Promise<App[]>
+  readLastAppsInfo(): Promise<App[]>
+  getDiffDownloadCount(prevApp: App[], curApp: App[]): TAppDiff
 }
