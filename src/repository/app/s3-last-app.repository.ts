b10252521ca6d3b7667f2c8c6554628f624010f2
milkcoke <mbh023@gmail.com>
2023-07-16 00:49:19 +0900

    refactor: Remove unnecessary method on s3 last app repository

--- src/repository/app/s3-last-app.repository.ts
+++ src/repository/app/s3-last-app.repository.ts
@@ -55,14 +55,4 @@ export class S3LastAppRepository implements ILastAppRepository {
       ContentType: 'application/json'
     }))
   }
-
-  private _streamToString(stream): Promise<string> {
-    return new Promise((resolve, reject)=>{
-      const chunks = []
-      stream.on('data', chunk=>chunks.push(chunk))
-      stream.on('error', reject)
-      stream.on('end', resolve(Buffer.concat(chunks).toString('utf8')))
-    })
-
-  }
 }
